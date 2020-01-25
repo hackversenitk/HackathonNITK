dd3c23d6b506a174051d1838b51b37a5657ce41b
Prakash-sa <sainiprakash525@gmai.com>
2020-01-25 17:43:20 +0530

    Making new Activities.

---
+++ app/src/main/java/com/example/hackathonnitk/ui/SigninUp.java
@@ -0,0 +1,16 @@
+package com.example.hackathonnitk.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hackathonnitk.R;
+
+public class SigninUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_up);
+    }
+}
