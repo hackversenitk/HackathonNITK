5505ef8dda9b12c5f8948fbc9418b6223b757d30
Prakash-sa <sainiprakash525@gmai.com>
2020-01-26 09:45:29 +0530

    Applied Notification.

--- app/src/main/java/com/example/hackathonnitk/ConstantsIt.java
+++ app/src/main/java/com/example/hackathonnitk/ConstantsIt.java
@@ -3,6 +3,9 @@ package com.example.hackathonnitk;
 public class ConstantsIt {
     public static final String LOCALURLIMAGEUPLOAD ="http://192.168.42.241:5002/store" ;
     public static final String LOCALURLGETFILENAME ="http://192.168.42.241:5002/all_images" ;
+    public static final String LOCALURLADDPEOPLE ="http://192.168.42.241:5002/add_files" ;//add people
+    public static final String LOCALURLGETALLFILENAME ="http://192.168.42.241:5002/public_images" ;//get to all public files
+    public static final String LOCALURLPUTALLFILENAME ="http://192.168.42.241:5002/make_public" ;//put to all public files
     public static final String LOCALURLGETIMAGE ="http://192.168.42.241:5002/send" ;
     public static final String LOCALURLDELETEIMAGE ="http://192.168.42.241:5002/delete" ;
     public static final String LOCALURLSENDVIDEO ="http://192.168.42.241:5002/vid_download" ;
